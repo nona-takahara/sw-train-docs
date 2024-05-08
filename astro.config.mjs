17b587f95b98e80f0e398c3dd94d70cd3e0b8fb6
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2024-05-08 13:25:38 +0900

    mermaidを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import remarkMermaid from '@southball/remark-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,10 +10,7 @@ export default defineConfig({
         format: "file"
     },
     markdown: {
-
-        remarkPlugins: [remarkMath],
-
+        remarkPlugins: [remarkMath, remarkMermaid],
         rehypePlugins: [rehypeKatex],
-
     }
 });
