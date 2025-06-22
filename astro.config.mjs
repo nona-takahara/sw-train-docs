9d2f0a7841012b523733cc01b11ea005f2034afe
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2025-06-23 02:07:03 +0900

    ActionsでChromeインストール

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-//import remarkMermaid from '@southball/remark-mermaid';
+import remarkMermaid from '@southball/remark-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
         format: "file"
     },
     markdown: {
-        remarkPlugins: [remarkMath],
+        remarkPlugins: [remarkMath, remarkMermaid],
         rehypePlugins: [rehypeKatex],
     }
 });
